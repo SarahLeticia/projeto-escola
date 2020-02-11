20aca7205a88fac0c38a2e3b58cfab6b7f35a92d
val_resco <60904408+Val-resco@users.noreply.github.com>
2020-02-11 15:53:06 -0300

    Update banco-de-dados.sql

--- banco-de-dados.sql
+++ banco-de-dados.sql
@@ -1,1 +1,25 @@
+--
+-- Estrutura da tabela `ingredientes`
+--
+
+CREATE TABLE INGREDIENTES (
+  id_ing int(11) NOT NULL,
+  nome varchar(100) CHARACTER SET utf8 NOT NULL,
+  qtd int(11) NOT NULL
+  primary key (id_ing)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cardapio`
+--
+CREATE TABLE CARDAPIO (
+  id_card int(11) NOT NULL,
+  turno varchar(50) CHARACTER SET utf8 NOT NULL,
+  item varchar(50) CHARACTER SET utf8 NOT NULL,
+  qtd int(11) NOT NULL,
+  cod_item int(11) NOT NULL
+  primary key (id_card)
+);
 
